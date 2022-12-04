a5b3689e6a46a6c7a74e66cd3591597efa10aa07
ryanajacobpitogo <ryanjacob.pitogo.20@usjr.edu.ph>
2022-12-04 17:50:55 +0800

    Initial constant commit

---
+++ healthily_app/lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Color varianbles
+const clrDarkRed = Color.fromARGB(255, 174, 38, 62);
+const clrLightRed = Color.fromARGB(255, 255, 51, 73);
+const clrMainRed = Color.fromARGB(255, 174, 38, 62);
+const clrPaleRed = Color.fromARGB(255, 202, 78, 77);
+const clrCyanBlue = Color.fromARGB(255, 17, 151, 245);
+
+//Size variables
+
+//Data list
