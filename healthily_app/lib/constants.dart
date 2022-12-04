c5b8d0a2f7527e9f50d9d354f1af168bb77667d6
ryanajacobpitogo <ryanjacob.pitogo.20@usjr.edu.ph>
2022-12-04 21:40:11 +0800

    Implemented input field and updated main and consts

--- healthily_app/lib/constants.dart
+++ healthily_app/lib/constants.dart
@@ -7,6 +7,7 @@ const clrMainRed = Color.fromARGB(255, 174, 38, 62);
 const clrPaleRed = Color.fromARGB(255, 202, 78, 77);
 const clrCyanBlue = Color.fromARGB(255, 17, 151, 245);
 const clrWhite = Colors.white;
+const clrGrey = Colors.grey;
 
 
 
@@ -18,7 +19,14 @@ const loginMinWidth = 120.0;
 final loginFacebookIcon = Image.asset("assets/facebook.png", width: 30,height: 30);
 final loginGoogleIcon = Image.asset("assets/google.png", width: 20,height: 20,);
 
-
+//Input Field constants
+const inputFieldBorRad = 15.0;
+const inputFieldFontSize = 15.0;
+const inputFieldPadding = 12.0;
+const inputFieldBorWidth = 2.5;
+//Input Field variables
+TextEditingController uNameController = TextEditingController();
+TextEditingController passController = TextEditingController();
 
 //
 //  
